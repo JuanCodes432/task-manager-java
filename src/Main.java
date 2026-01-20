aa27dc90bd184097291be1c706b5bf56f580169b
JuanCodes432 <juancodeslogic@gmail.com>
2026-01-20 09:58:25 -0500

    Mejorar interaccion por consola y validacion al completar tareas

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,66 @@ import service.GestorTareas;
 import util.ArchivoUtil;
 import model.Tarea;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+    private static final Scanner entrada = new Scanner(System.in);
 
+    public static int menuInterfaz(){
+        System.out.println("\n Menu");
+        System.out.println("1. Agregar Tarea");
+        System.out.println("2. Listar Tareas");
+        System.out.println("3. Completar Tarea");
+        System.out.println("4.Salir");
+        System.out.print("Ingrese una opcion: ");
+        int opcion = entrada.nextInt();
+        System.out.println("");
+        return opcion;
+    }
     public static void main(String[] args) {
+        GestorTareas gestor = new GestorTareas();
+        gestor.setTareas(ArchivoUtil.cargarTareas());
+        int opcion;
+        boolean salir = false;
+        while(!salir){
+            opcion = menuInterfaz();
+            switch(opcion){
+                case 1:
+                    System.out.print("Ingrese la prioridad de la tarea: ");
+                    int prioridad = entrada.nextInt();
+                    entrada.nextLine();
+                    System.out.println("");
+                    System.out.print("Ingrese una descripcion de la tarea: ");
+                    String descripcion = entrada.nextLine();
+                    System.out.println("");
+                    gestor.agregarTarea(descripcion,prioridad);
+                    break;
 
-        // 1️⃣ Cargar tareas desde archivo
-        ArrayList<Tarea> tareasCargadas = ArchivoUtil.cargarTareas();
+                case 2:
+                    gestor.listarTareas();
+                    break;
 
-        // 2️⃣ Crear gestor con tareas cargadas
-        GestorTareas gestor = new GestorTareas(tareasCargadas);
+                case 3:
+                    System.out.println("Ingrese la posicion de la tarea que desea completar");
+                    int index = entrada.nextInt();
+                    if(index <= 0 || index > gestor.getTareas().size()){
+                        System.out.println("Esta tarea no existe");
 
-        // 3️⃣ Usar el sistema
-        gestor.agregarTarea("Estudiar Java", 1);
-        gestor.agregarTarea("Subir proyecto a GitHub", 2);
+                    }else {
+                        gestor.completarTarea(index);
+                    }
+                    break;
 
-        System.out.println("📋 Lista de tareas:");
-        gestor.listarTareas();
+                case 4:
+                    System.out.println("Contenido guardado con exito");
+                    ArchivoUtil.guardarTareas(gestor.getTareas());
+                    salir = true;
+                    break;
 
-        gestor.completarTarea(1);
-
-        System.out.println("\n Lista actualizada:");
-        gestor.listarTareas();
-
-        //  Guardar tareas antes de salir
-        ArchivoUtil.guardarTareas(gestor.getTareas());
-
-        System.out.println("\n Tareas guardadas correctamente.");
+                default:
+                    System.out.println("Opción no válida, intente de nuevo.");
+                    break;
+            }
+        }
     }
 }
--- src/service/GestorTareas.java
+++ src/service/GestorTareas.java
@@ -6,8 +6,8 @@ import java.util.ArrayList;
 public class GestorTareas {
     private ArrayList<Tarea> tareas;
 
-    public GestorTareas(ArrayList<Tarea> tareas){
-        this.tareas = tareas;
+    public GestorTareas(){
+        tareas = new ArrayList<>();
     }
 
     //Se encarga de agregar tareas
@@ -18,14 +18,19 @@ public class GestorTareas {
     }
 
     public void listarTareas(){
+        if(tareas.size() == 0){
+            System.out.println("No hay tareas disponibles");
+            return;
+        }
+        System.out.println("Lista de tareas disponibles");
         for (int i = 0; i < tareas.size(); i++) {
-            System.out.println(i + ". " + tareas.get(i));
+            System.out.println((i+1) + ". " + tareas.get(i));
         }
     }
 
     //Marca tarea como completada
     public void completarTarea(int index){
-        if(index >= 0 && index < tareas.size()){
+        if(index > 0 && index <= tareas.size()){
             System.out.println("Tarea completada");
             tareas.get(index-1).setCompletada(true);
         }else{
@@ -35,5 +40,9 @@ public class GestorTareas {
 
     public ArrayList<Tarea> getTareas(){
         return tareas;
+    }
+
+    public void setTareas(ArrayList<Tarea> tareas){
+        this.tareas = tareas;
     }
 }
